c7cf7556f62a483e34b0a24280c13f1b5646efbf
Polina Matavina <paulin.mipt@gmail.com>
2018-04-21 23:58:23 +0300

    ui update

--- app.py
+++ app.py
@@ -1,36 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from requests import get
 from urllib.parse import urlencode
 from PIL import Image
 from io import BytesIO
 
+from ip_selector import Org, select_ip
+
 app = Flask(__name__, static_folder='static')
-
-
-MAPVIEW_URL = 'https://image.maps.cit.api.here.com/mia/1.6/mapview'
-
-'''
-def get_map_img():
-    params = urlencode(
-    {
-        'vt': 1,
-        'z': 2,
-        'w': 1600,
-        'h': 900,
-    })
-    response = get('{}?{}'.format(MAPVIEW_URL, params))
-    if response.status_code != 200:
-        raise Exception("bad response code while map retrieval: code {}".format(response.status_code))
-    img = Image.open(BytesIO(response.content))
-    name = "tmpimg" + ".jpg"
-    img.save("static/" + name, "JPEG")
-    return name
-'''
-
 
 @app.route('/')
 def draw_map():
-    # global_map_name = get_map_img()
     return render_template('index.html')
 
 
