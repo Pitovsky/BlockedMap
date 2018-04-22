280d5ebb4303a0bc5422cab67bc93364ed54d9fd
Alexey Neganov <neganovalexey@gmail.com>
2018-04-22 03:24:53 +0300

    circles on map

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from requests import get
 from urllib.parse import urlencode
 from PIL import Image
@@ -8,10 +8,11 @@ from datetime import datetime
 from ip_selector import Org, select_ip
 
 app = Flask(__name__, static_folder='static')
+gps = []
 
 @app.route('/', methods=['GET'])
 def draw_map():
-    return render_template('index.html')
+    return render_template('index.html', points=gps)
 
 @app.route('/filter',  methods=['POST'])
 def make_info():
@@ -28,9 +29,8 @@ def make_info():
         else:
             orgs.append(getattr(Org, key))
     gps = select_ip(orgs, ts_low, ts_high)
-    for gp in gps:
-        print(gp)
-    return ""
+    return render_template('index.html', points=gps)
+    # return redirect("")
 
 if __name__ == '__main__':
     app.run()
