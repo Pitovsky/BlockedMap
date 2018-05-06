ddfb7b1411a940396a8acf548589f8e5a5917b76
paulin-mipt <paulin.mipt@gmail.com>
2018-05-06 14:42:14 +0000

    parse date only

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def make_info():
     for key in data:
         if key == "range":
             tss = data[key].split(" - ")
-            ts_low = datetime.strptime(tss[0], "%B %d, %Y")
-            ts_high = datetime.strptime(tss[1], "%B %d, %Y")
+            ts_low = datetime.strptime(tss[0], "%B %d, %Y").date()
+            ts_high = datetime.strptime(tss[1], "%B %d, %Y").date()
         else:
             orgs.append(getattr(Org, key))
     gps = [{'lat': p[0] if p[0] else 0, 
